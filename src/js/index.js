872280b6de08220a694c126266c0337e5010ac0d
Arek <arkadiuszflorczak993@gmail.com>
2021-04-19 23:57:01 +0200

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,35 @@
 /* Here goes your JS code */
+const formValidate = form => {
+	const email = form.elements['email'];
+	const password = form.elements['password'];
+	const rules = form.elements['rules-checkbox'];
+
+	if(!/^.+@.+\..+$/.test(email.value)) {
+			email.setCustomValidity("Invalid email address");
+	} else {
+			email.setCustomValidity('');
+	}
+
+	if(password.value.length < 8) {
+			password.setCustomValidity('Password must contain at least 8 characters');
+	} else {
+			password.setCustomValidity('');
+	}
+
+	if(!rules.checked) {
+			rules.setCustomValidity("Please check this box if you want to proceed");
+	} else {
+			rules.setCustomValidity("");
+	}
+	
+	form.reportValidity();
+}
+
 document.addEventListener('DOMContentLoaded', (e) => {
 	const popup = document.querySelector('.popup');
 	const showPopupBtn = document.querySelector('#show-popup-form');
 	const closeBtn = document.querySelector('.popup__close-btn');
+	const form = document.querySelector('#login-form');
 
 	showPopupBtn.addEventListener('click', () => {
 		popup.classList.add('popup--show');
@@ -10,5 +37,10 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
 	closeBtn.addEventListener('click', () => {
 		popup.classList.remove('popup--show');
+	});
+
+	form.addEventListener('submit', e => {
+		e.preventDefault();
+		formValidate(form);
 	});
 });
