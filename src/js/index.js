ce910e5c9fdbea57410262635e33a102e78b39b3
Arek <arkadiuszflorczak993@gmail.com>
2021-04-19 22:57:04 +0200

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 /* Here goes your JS code */
+document.addEventListener('DOMContentLoaded', (e) => {
+	const popup = document.querySelector('.popup');
+	const showPopupBtn = document.querySelector('#show-popup-form');
+	const closeBtn = document.querySelector('.popup__close-btn');
+
+	showPopupBtn.addEventListener('click', () => {
+		popup.classList.add('popup--show');
+	});
+
+	closeBtn.addEventListener('click', () => {
+		popup.classList.remove('popup--show');
+	});
+});
